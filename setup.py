a0a5a72e5b2b932679cc28a81d6e8a38ea842d55
Vincent Monet <vincent@dacker.co>
2019-10-18 11:14:16 +0200

    monitoring

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='pyspreadsheet',
-    version='0.1.5',
+    version='0.1.6',
     description='Easily send data to Google Sheets',
     long_description=readme,
     author='Dacker',
@@ -16,7 +16,7 @@ setup(
     python_requires='>=3',
     install_requires=[
         "googleauthentication>=0.0.8",
-        "dbstream>=0.0.10",
+        "dbstream>=0.0.11",
         "google-api-python-client>=1.6.6"
     ],
 )
