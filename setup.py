cc65ed3d9b6f124dc336776f263f2eb9ffba3d32
Pierre-François Lucet <pierre-francois@dacker.co>
2023-04-20 10:34:09 +0200

    set pygsheets 2.0.2 version back

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='pyspreadsheet',
-    version='0.2.13',
+    version='0.2.14',
     description='Easily send data to Google Sheets',
     long_description=readme,
     author='Dacker',
@@ -18,7 +18,7 @@ setup(
         "googleauthentication>=0.0.10",
         "dbstream>=0.1.2",
         "google-api-python-client>=1.6.6",
-        "pygsheets>=2.0.2",
+        "pygsheets==2.0.2",
         "pyyaml>=5.3.1",
         "google-auth>=0.4.8",
         "jinja2>=2.11.2"
